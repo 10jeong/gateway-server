531677d367886866a85a9479b9562456f4d8f2f2
Yoonho Cha <0119hoya@naver.com>
2026-05-01 13:46:22 +0900

    feat: common-module의존성 분리를 위한 코드 수정

--- src/main/java/com/yeoljeong/tripmate/response/GatewayResponseUtil.java
+++ src/main/java/com/yeoljeong/tripmate/response/GatewayResponseUtil.java
@@ -1,6 +1,6 @@
 package com.yeoljeong.tripmate.response;
 
-import com.yeoljeong.tripmate.exception.constants.ErrorCode;
+import com.yeoljeong.tripmate.error.GatewayErrorCode;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -8,7 +8,7 @@ import java.nio.charset.StandardCharsets;
 
 public class GatewayResponseUtil {
 
-    public static Mono<Void> writeErrorResponse(ServerWebExchange exchange, ErrorCode errorCode) {
+    public static Mono<Void> writeErrorResponse(ServerWebExchange exchange, GatewayErrorCode errorCode) {
         exchange.getResponse().setStatusCode(errorCode.getStatus());
         byte[] bytes = errorCode.getMessage().getBytes(StandardCharsets.UTF_8);
         return exchange.getResponse().writeWith(
